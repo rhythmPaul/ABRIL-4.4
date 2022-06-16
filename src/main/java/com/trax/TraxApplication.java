e62cd2414078498608730f4bd15dbfb90efbd428
rhythmPaul <rhythmpaul1999@gmail.com>
2022-06-17 01:50:16 +0530

    Developed Following Modules 1. Lead, 2. Contacts, 3. Billing, 4. Email

---
+++ src/main/java/com/trax/TraxApplication.java
@@ -0,0 +1,13 @@
+package com.trax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication							// This is the file where the execution begin of the spring boot projects & that has a main method
+public class TraxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraxApplication.class, args);
+	}
+
+}
